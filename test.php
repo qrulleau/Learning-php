987abb496f57f2ec6d0aa867a2b536f96e8691a0
qrulleau <quentin.rulleau@yahoo.com>
2021-02-10 12:27:53 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php 
+#introduction au variable , concaténation et difference simple / double quotes
+$prenom = 'Mard'; 
+$nom = 'Doe';
+$note1 = '10';
+$note2 = '20';
+
+echo 'Bonjour '.$prenom.' '.$nom.' '.'vous avez eu '.($note1 + $note2)/2 ."\n" ;
+
+#introduction au tableau
